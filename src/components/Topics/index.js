5a6ff5aa21a0b7915ab3e8808ad7a0723e7e6ca8
capriceebanks <capri.ee@outlook.com>
2022-03-14 17:14:02 +0000

    topics section

--- src/components/Topics/index.js
+++ src/components/Topics/index.js
@@ -5,10 +5,17 @@ const Topics = () => {
 return(
 <>
 <FormControl>
-  <FormLabel htmlFor='country'>Country</FormLabel>
-  <Select id='country' placeholder='Select country'>
-    <option>United Arab Emirates</option>
-    <option>Nigeria</option>
+  <FormLabel htmlFor='topics'>Topics</FormLabel>
+  <Select id='topics' placeholder='Pick a topic!'>
+    <option>All</option>
+  </Select>
+  <FormLabel htmlFor='difficulty'>Difficulty</FormLabel>
+  <Select id='difficulty' placeholder='Select difficulty'>
+    <option>All</option>
+    <FormLabel htmlFor='timelimit'>Timelimit</FormLabel>
+  <Select id='timelimit' placeholder='How long?'>
+    <option>All</option>
+  </Select>
   </Select>
 </FormControl>
 </>
