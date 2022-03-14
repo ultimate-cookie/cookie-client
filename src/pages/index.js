73b16498ead9d76d89cfcf7e9397a32e7a79833d
rwclutterbuck <rwclutterbuck@outlook.com>
2022-03-14 12:27:46 +0000

    Add: pages file structure

---
+++ src/pages/index.js
@@ -0,0 +1,7 @@
+export { default as Home } from "./Home";
+export { default as Game } from "./Game";
+export { default as Host } from "./Host";
+export { default as Guest } from "./Guest";
+export { default as Roundup } from "./Roundup";
+export { default as JoinLobby } from "./JoinLobby";
+export { default as CreateLobby } from "./CreateLobby";
