a3a4deb341079120283348e413eb1716108a1087
Sukhraj Bhandal <sukhrajbhandal1998@gmail.com>
2022-03-16 11:05:47 +0000

    merge conflict part 2

--- src/App.js
+++ src/App.js
@@ -1,35 +1,50 @@
-import React from 'react';
+import React from "react";
 // import { Switch, Route } from 'react-router-dom';
-import '@chakra-ui/react'
+import "@chakra-ui/react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
-import { 
-	// JoinButton, 
-	// CreateButton, 
-	CreateLobby,
-	JoinLobby, 
-	QuizDetails, 
-	Welcome,
-	// NavBar 
-} from './components';
+import {
+  // JoinButton,
+  // CreateButton,
+  TopBar,
+  Roundup,
+  CreateLobby,
+  JoinLobby,
+  QuizDetails,
+  Welcome,
+  PlayerList,
+  Quiz,
+} from "./components";
 
 function App() {
-return (
-<>
-	{/* <NavBar /> */}
-	<CreateLobby />
-	<JoinLobby />
-	<QuizDetails />
-	<Welcome />
-	{/* <div>
-		<NavBar />
-				<Switch>
-						<Route exact path="/" />
-						<Route path="/abc" />
-						<Route path="/xyz" />
-				</Switch>
-	</div> */}
-</>
-)
+  return (
+    <>
+      <TopBar />
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Welcome />
+          </Route>
+          <Route path="/create">
+            <CreateLobby />
+            <QuizDetails />
+          </Route>
+          <Route path="/join">
+            <JoinLobby />
+          </Route>
+          <Route path="/lobby">
+            <PlayerList />
+          </Route>
+          <Route path="/play">
+            <Quiz />
+          </Route>
+          <Route path="/endScreen">
+            <Roundup />
+          </Route>
+        </Switch>
+      </Router>
+    </>
+  );
 }
 
 export default App;
