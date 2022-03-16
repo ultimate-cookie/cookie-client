538e5cd1b799df00c3586141e58ca645e327dd38
Sukhraj Bhandal <sukhrajbhandal1998@gmail.com>
2022-03-16 11:00:39 +0000

    fixed merge conflicts

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-
-import store from './store';
 import App from './App';
+import { ChakraProvider } from '@chakra-ui/react'
+import { BrowserRouter as Router } from 'react-router-dom';
 
 ReactDOM.render(
-    <React.StrictMode>
-        <Provider store={store}>
-                <App />
-        </Provider>
-    </React.StrictMode>,
+  <ChakraProvider>
+    <Router>
+      <App />
+    </Router>
+  </ChakraProvider>,
 document.getElementById("root"));
