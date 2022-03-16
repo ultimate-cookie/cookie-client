562ea3e877ea6b40097f002b943ed74a4b1f9648
rwclutterbuck <rwclutterbuck@outlook.com>
2022-03-16 10:39:48 +0000

    Fix: links to lobby page

--- src/components/CreateLobby/index.js
+++ src/components/CreateLobby/index.js
@@ -1,35 +1,57 @@
-import React from 'react';
-import { CreateButton } from '../index'
-import {
-  FormControl,
-  FormLabel,
-  Input,
-  Center,
-} from '@chakra-ui/react'
-import { Container, Heading } from '@chakra-ui/layout';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { CreateButton } from "../index";
+import { FormControl, FormLabel, Input, Center } from "@chakra-ui/react";
+import { Container, Heading } from "@chakra-ui/layout";
+import { Link } from "react-router-dom";
 
 const CreateLobby = () => {
-return(
-<>
-<Center mt={10}>
-  <Container id='create' w="600px" rounded='xl' boxShadow='lg' bg="#f78888" m='3' p='7' mt='10'>
-    <Center>
-      <Heading>Create Lobby</Heading>
-    </Center>
-    <FormControl>
-      <FormLabel htmlFor='username'></FormLabel>
-      <Input isRequired className='username' placeholder='Username' bg="#ffd0d0"/>
-      <FormLabel htmlFor='password'></FormLabel>
-      <Input isRequired className='password' placeholder='Password' bg="#ffd0d0"/>
-      <FormLabel htmlFor='lobbyname'></FormLabel>
-      <Input isRequired className='lobbyname' placeholder='Lobby Name' bg="#ffd0d0" />
-      <Center>
-        <CreateButton />
+  return (
+    <>
+      <Center mt={10}>
+        <Container
+          id="create"
+          w="600px"
+          rounded="xl"
+          boxShadow="lg"
+          bg="#f78888"
+          m="3"
+          p="7"
+          mt="10"
+        >
+          <Center>
+            <Heading>Create Lobby</Heading>
+          </Center>
+          <FormControl>
+            <FormLabel htmlFor="username"></FormLabel>
+            <Input
+              isRequired
+              className="username"
+              placeholder="Username"
+              bg="#ffd0d0"
+            />
+            <FormLabel htmlFor="password"></FormLabel>
+            <Input
+              isRequired
+              className="password"
+              placeholder="Password"
+              bg="#ffd0d0"
+            />
+            <FormLabel htmlFor="lobbyname"></FormLabel>
+            <Input
+              isRequired
+              className="lobbyname"
+              placeholder="Lobby Name"
+              bg="#ffd0d0"
+            />
+            <Center>
+              <Link to="/lobby">
+                <CreateButton />
+              </Link>
+            </Center>
+          </FormControl>
+        </Container>
       </Center>
-    </FormControl>
-  </Container>
-</Center>
-</>
-)};
+    </>
+  );
+};
 export default CreateLobby;
