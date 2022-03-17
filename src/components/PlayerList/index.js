3d5e05d9a4c6bcd6f841acbe71013f29604240c5
rwclutterbuck <rwclutterbuck@outlook.com>
2022-03-17 16:25:50 +0000

    Add: list of players in lobby

--- src/components/PlayerList/index.js
+++ src/components/PlayerList/index.js
@@ -2,29 +2,49 @@ import { Center, Container, Heading } from "@chakra-ui/react";
 import React, { useState } from "react";
 import { Redirect } from "react-router-dom";
 import { Player, StartButton } from "../index";
+import { Text } from "@chakra-ui/react";
 
 const PlayerList = ({ socket }) => {
   const [redirect, setRedirect] = useState(false);
+  const [players, setPlayers] = useState([]);
   const startQuiz = () => {
     socket.emit("startQuiz", "Please start the quiz");
     setRedirect(true);
   };
+  socket.on("lobbyPlayers", (playerList) => {
+    setPlayers(playerList);
+  });
+
+  const renderPlayers = () => {
+    const playerList = [];
+    for (const player of players) {
+      playerList.push(
+        <Text bg="#ffd0d0" key={player}>
+          {player}
+        </Text>
+      );
+    }
+    return <>{playerList}</>;
+  };
+
   return (
     <>
       {redirect ? <Redirect to="/play" /> : undefined}
       <Center mt={10}>
-        <Container id="play"
+        <Container
+          id="play"
           w="600px"
           rounded="xl"
           boxShadow="lg"
           bg="#f78888"
           m="3"
           p="7"
-          mt="10">
-            <Center>
-              <Heading p={3}>Player List</Heading>
-            </Center>
-          <Player />
+          mt="10"
+        >
+          <Center>
+            <Heading p={3}>Player List</Heading>
+          </Center>
+          {renderPlayers()}
           <Center>
             <StartButton startQuiz={startQuiz} />
           </Center>
