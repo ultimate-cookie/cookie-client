be26341094c0536defc2595f3c101c84e867fd74
capriceebanks <capri.ee@outlook.com>
2022-03-17 17:26:46 +0000

    dark mode button fully functioning

--- src/theme.js
+++ src/theme.js
@@ -1,8 +1,8 @@
-
 import { extendTheme } from '@chakra-ui/react'
+import { mode } from '@chakra-ui/theme-tools'
 
 const config = {
-  initialColorMode: '#ffe4d5'|'#3C3741',
+  initialColorMode: 'system',
   useSystemColorMode: false,
   fonts: { 
     heading: 'Patrick Hand, sans-serif', 
@@ -11,6 +11,16 @@ const config = {
   
 }
 
-const theme = extendTheme({ config })
+const theme = extendTheme({ 
+  config, 
+  styles: {
+    global: (props) => ({
+      body: {
+        bg: mode('#ffe4d5','#3C3741')
+        (props),
+      }
+    })
+  }
+})
 
 export default theme
