799d4c12dce8cd155c37cc03f3b5eb581c0b624d
rwclutterbuck <rwclutterbuck@outlook.com>
2022-03-18 12:15:03 +0000

    Fix: merge conflict from not saving vs code file

--- src/components/PlayerList/index.js
+++ src/components/PlayerList/index.js
@@ -19,15 +19,10 @@ const PlayerList = ({ socket }) => {
     const playerList = [];
     for (const player of players) {
       playerList.push(
-<<<<<<< HEAD
-        <Box bg={person}>
-          <Text key={player}>{player}</Text>
-=======
         <Box bg={person} rounded="xl">
           <Center>
             <Text key={player}>{player}</Text>
           </Center>
->>>>>>> 0dea1ef8ad6536bc61e9e6b274788f2fff3666ed
         </Box>
       );
     }
@@ -54,11 +49,7 @@ const PlayerList = ({ socket }) => {
           <Center>
             <Heading p={3}>Player List</Heading>
           </Center>
-<<<<<<< HEAD
-          <SimpleGrid columns={2} spacing={5}>
-=======
           <SimpleGrid p={6} columns={2} spacing={5}>
->>>>>>> 0dea1ef8ad6536bc61e9e6b274788f2fff3666ed
             {renderPlayers()}
           </SimpleGrid>
           <Center pt={8}>
